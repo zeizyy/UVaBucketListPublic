6b441f19b5bc58945fef4f9e37764ce0c659a9dd
Qian Xiong <qx2dk@virginia.edu>
2016-02-04 00:09:12 -0500

    Dynamically generate the list from the csv file

--- app/src/main/java/com/uva/vivian/bucketlist_lxz/MainActivity.java
+++ app/src/main/java/com/uva/vivian/bucketlist_lxz/MainActivity.java
@@ -1,18 +1,23 @@
 package com.uva.vivian.bucketlist_lxz;
 
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -52,6 +57,33 @@ public class MainActivity extends AppCompatActivity {
 
         final TextView textView2 = (TextView) findViewById(R.id.textView2);
         textView2.setOnClickListener(onClickListener);
+
+        // read csv file to dynamically generate the list
+        InputStream inputStream = getResources().openRawResource(R.raw.uva111things);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = null;
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.listLinearLayout);
+        try {
+            line = reader.readLine();
+            while (line != null) {
+                CheckBox checkBox = new CheckBox(this);
+
+                TextView tv = new TextView(this);
+                tv.setText(line);
+                tv.setOnClickListener(onClickListener);
+                tv.setGravity(Gravity.CENTER_VERTICAL);
+
+                LinearLayout oneRow = new LinearLayout(this);
+                oneRow.addView(checkBox);
+                oneRow.addView(tv);
+
+                linearLayout.addView(oneRow);
+                line = reader.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
