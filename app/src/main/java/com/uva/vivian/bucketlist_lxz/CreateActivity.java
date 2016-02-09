fb397635ad24a787b695f8aafcca3a94599c915b
Qian Xiong <qx2dk@virginia.edu>
2016-02-08 21:47:19 -0500

    Renamed views in xml; limit title input to one line; changed layout to LinearLayout

--- app/src/main/java/com/uva/vivian/bucketlist_lxz/CreateActivity.java
+++ app/src/main/java/com/uva/vivian/bucketlist_lxz/CreateActivity.java
@@ -7,13 +7,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class CreateActivity extends AppCompatActivity {
     static BucketOpenHelper db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,19 +33,19 @@ public class CreateActivity extends AppCompatActivity {
         db = new BucketOpenHelper(this);
     }
 
-    public void createBucket(View view){
+    public void createBucket(View view) {
         Context context = this.getApplicationContext();
-        EditText nameField = (EditText) findViewById(R.id.editText);
+        EditText nameField = (EditText) findViewById(R.id.editText_create_title);
         String name = nameField.getText().toString();
-        EditText descriptionField = (EditText) findViewById(R.id.editText2);
+        EditText descriptionField = (EditText) findViewById(R.id.editText_create_description);
         String description = descriptionField.getText().toString();
-        if(name.isEmpty()|| description.isEmpty()) {
-            Toast.makeText(context,"Missing required field(s)", Toast.LENGTH_SHORT).show();
+        if (name.isEmpty() || description.isEmpty()) {
+            Toast.makeText(context, "Missing required field(s)", Toast.LENGTH_SHORT).show();
             return;
         }
         Bucket bucket = new Bucket(name, description);
         db.insertBucket(bucket);
-        Toast.makeText(context,"New Bucket Created!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, "New Bucket Created!", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(context, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
