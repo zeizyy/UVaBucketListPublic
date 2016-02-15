ae763074cbf530f15bc8be0296837e7605306de6
Vivian Liu <wl2nw@virginia.edu>
2016-02-14 19:52:33 -0500

    change threshhold

--- app/src/main/java/com/uva/vivian/bucketlist_lxz/OnSwipeTouchListener.java
+++ app/src/main/java/com/uva/vivian/bucketlist_lxz/OnSwipeTouchListener.java
@@ -44,7 +44,7 @@ public class OnSwipeTouchListener implements OnTouchListener {
                 float diffY = e2.getY() - e1.getY();
                 float diffX = e2.getX() - e1.getX();
                 if (Math.abs(diffX) > Math.abs(diffY)) {
-                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (Math.abs(diffX) > SWIPE_THRESHOLD-30 && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD-20) {
                         if (diffX > 0) {
                             onSwipeRight();
                         } else {
